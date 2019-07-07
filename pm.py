59026d1870671013500bc7876100f66bc2c2589d
Kevin Leo <lgx@live.com>
2019-07-07 20:38:53 +0800

    1. 代码规范检查 2. 实现 issue #3

--- pm.py
+++ pm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""Stupid Personnel Photo Manager"""
+
 import os
 import shutil
 import hashlib
@@ -8,21 +10,25 @@ from pathlib import Path
 from collections import Counter
 
 
-m_test = False
-m_verbose = False
+class RunMode():
+    """运行模式"""
+    test = False
+    verbose = False
 
 
 class DirHash():
+    """Dir Hash"""
+
     HASH_FILE = ".hash"
 
     def __init__(self, home):
-        self.home = Path(home)
+        """Init with path"""
+        self.home = Path(home).expanduser()
         self.hashfile = self.home / self.HASH_FILE
         self.hash2path = {}
         self.path2hash = {}
         self.dirty = False
         self._hash_load()
-        
 
     def _hash_load(self):
         """加载 Hash 列表"""
@@ -34,51 +40,48 @@ class DirHash():
                 if not line:
                     continue
                 md5, path = line.strip().split(maxsplit=1)
-                assert(md5 not in hash2path)
+                assert md5 not in hash2path
                 hash2path[md5] = path
                 path2hash[path] = md5
 
             self.hash2path = hash2path
             self.path2hash = path2hash
 
-        if m_verbose: print("Load %d hash from %s" % (len(hash2path), self.hashfile))
+        if RunMode.verbose:
+            print("Load %d hash from %s" % (len(hash2path), self.hashfile))
         return hash2path, path2hash
-
 
     def _hash_save(self):
         """Save hash"""
-        if not self.dirty or m_test:
+        if not self.dirty or RunMode.test:
             return True
 
         tmpfile = str(self.hashfile) + ".tmp"
-        with open(tmpfile, 'w') as fp:
+        with open(tmpfile, 'w') as fp_:
             for path, md5 in self.path2hash.items():
-                fp.write("%s %s\n" % (md5, path))
+                fp_.write("%s %s\n" % (md5, path))
 
         os.rename(tmpfile, self.hashfile)
         self.dirty = False
         return True
 
-
     def _hash_del_file(self, path):
         """从Hash数据库里删除一个目录下文件"""
-        assert(str(path) in self.path2hash)
-        md = self.path2hash.pop(path)
-        del self.hash2path[md]
+        assert str(path) in self.path2hash
+        md5 = self.path2hash.pop(path)
+        del self.hash2path[md5]
         self.dirty = True
         return True
 
-
     def _hash_add_file(self, path):
         """向Hash数据库里添加一个目录下文件"""
-        assert(str(path) not in self.path2hash)
-        p = self.home / path
-        md5 = hashlib.md5(p.read_bytes()).hexdigest()
+        assert str(path) not in self.path2hash
+        fullpath = self.home / path
+        md5 = hashlib.md5(fullpath.read_bytes()).hexdigest()
         self.path2hash[path] = md5
         self.hash2path[md5] = path
         self.dirty = True
         return True
-
 
     def _list_dir(self):
         """列出目录下所有文件"""
@@ -91,7 +94,7 @@ class DirHash():
                 if name == '.DS_Store':
                     try:
                         os.unlink(path)
-                    except:
+                    except OSError:
                         pass
                 if not name.startswith('.'):
                     files.append(path[2:])
@@ -99,49 +102,48 @@ class DirHash():
         os.chdir(cwd)
         return files
 
-
-    def exists(self, md):
+    def exists(self, md5):
         """一个Hash是否存在"""
-        return md in self.hash2path
+        return md5 in self.hash2path
 
-
-    def add_file(self, src, subdir, name, md):
+    def add_file(self, src, subdir, name, md5):
         """Add a file"""
-        if md in self.hash2path:
-            if m_verbose: print("ADD: ignore dupe file [%s]" % src)
+        if md5 in self.hash2path:
+            if RunMode.verbose:
+                print("ADD: ignore dupe file [%s]" % src)
             return 'dupe'
 
-        d = self.home / subdir 
-        d.mkdir(mode=0o700, parents=True, exist_ok=True)
+        apath = self.home / subdir
+        apath.mkdir(mode=0o700, parents=True, exist_ok=True)
         suffix = src.suffix.lower()
+        i = None
         for i in range(100):
-            t = d / ("%s%02d%s" % (name, i, suffix))
-            if not t.exists():
+            dst = apath / ("%s%02d%s" % (name, i, suffix))
+            if not dst.exists():
                 break
 
         if i == 100:
             print("ERR: to many filename collision")
             return 'err'
 
-        if m_test: return None
+        if RunMode.test:
+            return None
 
-        shutil.copy2(src, t)
-        path = t.relative_to(self.home)
-        self.hash2path[md] = str(path)
-        self.path2hash[str(path)] = md
+        shutil.copy2(src, dst)
+        path = dst.relative_to(self.home)
+        self.hash2path[md5] = str(path)
+        self.path2hash[str(path)] = md5
         self.dirty = True
         return None
 
- 
     def stat_suffix(self):
         """统计文件扩展名"""
-        cs = Counter()
-        for path in self.path2hash.keys():
+        counts = Counter()
+        for path in self.path2hash:
             suffix = path.rsplit('.', 1)[1]
-            cs[suffix] += 1
-        return cs
+            counts[suffix] += 1
+        return counts
 
-       
     def sync(self):
         """同步Hash文件和目录"""
         paths = set(self._list_dir())
@@ -150,36 +152,39 @@ class DirHash():
         removed = files - paths
         if removed:
             for path in removed:
-                if m_verbose: print(f"File {path} has been removed")
+                if RunMode.verbose:
+                    print(f"File {path} has been removed")
                 self._hash_del_file(path)
 
         added = paths - files
         if added:
             for path in added:
-                if m_verbose: print(f"Add file {path} to HashDB")
+                if RunMode.verbose:
+                    print(f"Add file {path} to HashDB")
                 self._hash_add_file(path)
 
         if any([added, removed]):
             self._hash_save()
         return len(removed), len(added)
 
-
     def __del__(self):
+        """对象销毁时自动保存"""
         self._hash_save()
 
 
 class PhotoHome(DirHash):
+    """Photo home"""
     pass
 
 
 def get_datetime(path, dts):
     """判断拍照时间"""
-    #GPS DateTime: 2015:04:01 02:55:15Z / 2015:04:11 09:10:42.65Z
+    # GPS DateTime: 2015:04:01 02:55:15Z / 2015:04:11 09:10:42.65Z
     if dts[-1]:
-        #UTC Time, 暂不转换为本地时间
+        # UTC Time, 暂不转换为本地时间
         dts[-1] = dts[-1].split('.')[0].replace('Z', '')
 
-    #删除时区
+    # 删除时区
     for i in range(len(dts) - 1):
         if not dts[i]:
             continue
@@ -194,14 +199,14 @@ def get_datetime(path, dts):
         print(f"W: {path} <> {dts}")
 
     if len(dts) > 3 and dts[3] and path.suffix in ('.mov', '.mp4'):
-        s = dts[3]
+        correct = dts[3]
     else:
-        s = min(values)
+        correct = min(values)
 
-    if m_verbose:
-        print(f"{path} -> {s}")
+    if RunMode.verbose:
+        print(f"{path} -> {correct}")
 
-    date, hms = s.split()
+    date, hms = correct.split()
     date = date.replace(':', '/')
     hms = hms.replace(':', '')
     return date, hms
@@ -209,30 +214,31 @@ def get_datetime(path, dts):
 
 def do_import(home, datefile, srcdir):
     """执行导入"""
-    cs = Counter()
+    counts = Counter()
     for line in datefile.open():
         line = line.strip()
         if not line:
             continue
 
-        path, md, *dts = line.rsplit(',')
-        if home.exists(md):
-            if m_verbose: print("Import: ignore dupe file %s" % path)
+        path, md5, *dts = line.rsplit(',')
+        if home.exists(md5):
+            if RunMode.verbose:
+                print("Import: ignore dupe file %s" % path)
             continue
 
         if srcdir:
-            p = srcdir / path
+            fullsrc = srcdir / path
         else:
-            p = Path(path)
+            fullsrc = Path(path)
 
-        day, hms = get_datetime(p, dts)
-        err = home.add_file(p, day, hms, md)
+        day, hms = get_datetime(fullsrc, dts)
+        err = home.add_file(fullsrc, day, hms, md5)
         if not err:
-            cs['ok'] += 1
+            counts['ok'] += 1
         else:
-            cs[err] += 1
+            counts[err] += 1
 
-    return cs
+    return counts
 
 
 def cmd_import(home, args):
@@ -264,21 +270,19 @@ def cmd_stat(home, _):
 
 def main(args):
     """Main function"""
-    global m_test, m_verbose
-
-    m_test = args.test
-    m_verbose = args.verbose
+    RunMode.test = args.test
+    RunMode.verbose = args.verbose
     home = PhotoHome(args.home)
     args.func(home, args)
 
 
 if __name__ == '__main__':
     def parse_cmdline():
-        """ cmd parse """
+        """Parse cmd line"""
         import argparse
 
         parser = argparse.ArgumentParser(description="Template Python App")
-        parser.add_argument('--home', default='/Volumes/RED/home', help='Photo home')
+        parser.add_argument('--home', default='~/.photos', help='Photo home')
         parser.add_argument('--test', action="store_true", help='Dryrun mode')
         parser.add_argument('--verbose', action="store_true", help='Verbose mode')
         subparsers = parser.add_subparsers(dest='cmd', required=True, help='sub-command help')
@@ -297,4 +301,3 @@ if __name__ == '__main__':
         return args
 
     main(parse_cmdline())
-
